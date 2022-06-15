1296fa82308b08838248fdf2605fffb5bea58cb9
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-15 16:37:04 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            QB qb1 = new QB(9350, 66.2, 839, 69, 8, 25, 65.6, 22.3, "Justin Herbert", );
+            {
+
+            }
+        }
+    }
+}
