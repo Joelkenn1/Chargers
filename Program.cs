fdfe9f316aac3259e25c4ec3b8f601a3fd72f962
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-16 00:30:26 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,97 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestProject1
-{
+namespace TestProject1 {
+     
      class Program
     {
         static void Main(string[] args)
         {
-            QB qb1 = new QB(9350, 66.2, 839, 69, 8, 25, 65.6, 22.3, "Justin Herbert", );
+            QB qb1 = new QB(9350, 66.2, 839, 69, 8, 25, 65.6, 22.3, "Justin Herbert", 23, 10, 6-6, 237, "Oregon");
             {
 
+            }
+
+            QB qb2 = new QB(63440, 64.9, 5277, 421, 3 209, 77.2, 16, "Philip Rivers", 29, 17, 6-5, 228, "NC State");
+            {
+
+            }
+
+            RB rb1 = new RB(3174, 13684, 145, 624, 31, 22, "LaDainian Tomlinson", 27, 21, 5-10, 215, "TCU");
+            {
+
+            }
+
+            RB rb2 = new RB(607, 2812, 21, 282, 11, 13.5, "Austin Ekeler", 26, 30, 5-10, 200, "Western St.");
+            {
+
+            }
+
+            TE te1 = new TE(541, 6741, 13, 45, 11, 12, "Kellen Winslow", 23, 80, 6-5, 251, "Missouri");
+            {
+
+            }
+
+            TE te2 = new TE(246, 2925, 12, 30, 2, 6, "Hunter Henry", 27, 86, 6-5, 250, "Arkansas");
+            {
+
+            }
+
+            WR wr1 = new WR(542, 10266, 19, 85, 5, 0, "Lance Alworth", 26, 19, 6-0, 184, "Arkansas");
+            {
+
+            }
+
+            WR wr2 = new WR(730, 8535, 12, 48, 13, 10, "Keenan Allen", 30, 13, 6-2, 211, "California");
+            {
+
+            }
+
+            CB cb1 = new CB(21, 631, 104, 1, 7 10, 0 ,0 "Quentin Jammer", 27, 23, 6-0, 204, "Texas");
+            {
+                
+            }
+
+            CB cb2 = new CB(24, 362, 69, 1, 2, 4, 0, 0, "Casey Hayward", 32, 26, 5-11, 192, "Vanderbilt");
+            {
+                
+            }
+
+            LB lb1 = new LB(18, 1077, 325, 1, 11, 18, 59, 0, "Junior Seau", 24, 55, 6-3, 250, "USC");
+            {
+
+            }
+
+            LB lb2 = new LB(3, 277, 108, 0, 15, 7, 51, 0, "Melvin Ingram", 32, 54 6-2, 247, "South Carolina");
+            {
+
+            }
+
+            S s1 = new S(34, 920, 286, 2, 15, 9 31, 0, "Rodney Harrison", 29, 37, 6-1, 220 "Western Illinois");
+            {
+
+            }
+
+            S s2 = new S(5, 173, 84, 0, 3, 0, 6, 0, "Derwin James", 25, 33, 6-2, 215, "Florida State");
+            {
+
+            }
+
+            DT dt1 = new DT(1, 345, 98, 1, 4, 3, 13, 0, "Jamal Williams", 29, 76, 6-3, 348, "Oklahoma State");
+            {
+
+            }
+
+            DE de1 = new DE(0, 213, 78, 0, 13, 3, 58, 0, "Joey Bosa", 26, 97, 6-5, 280, "Ohio");
+            {
+
+            }
+
+            DE de2 = new DE(3,700, 67, 1,21, 16, 133, 0, "Leslie O'Neal", 31, 91, 6-4, 275, "Oklahoma State");
+            {
+                
             }
         }
     }
 }
+
