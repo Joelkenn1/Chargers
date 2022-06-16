e8af40a4e94ed4651ddeb9242ce0d938f5632980
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-16 00:36:33 -0500

    Update WR.cs

--- WR.cs
+++ WR.cs
@@ -12,17 +12,16 @@ namespace TestProject1
         public int Yards = 0;
         public int Yards_After_Catch = 0;
         public int Touchdowns = 0;
-        public int Targets = 0;
         public int Fumbles = 0;
         public int Fantasy_Points = 0;
-        public WR(int catches, int yards, int yards_after_catch, int touchdowns, int targets, int fumbles, int fantasy_Points, string name, int age, int number, int height, int weight, int minutesplayed, string college)
-        : base(name, age, number, height, weight, minutesplayed, college)
+        
+        public WR(int catches, int yards, int yards_after_catch, int touchdowns, int fumbles, int fantasy_Points, string name, int age, int number, int height, int weight, string college)
+        : base(name, age, number, height, weight, college)
         {
             this.Catches = catches;
             this.Yards = yards;
             this.Yards_After_Catch = yards_after_catch;
             this.Touchdowns = touchdowns;
-            this.Targets = targets;
             this.Fumbles = fumbles;
             this.Fantasy_Points = fantasy_Points;
         }
