5ab3b7133538286ab0e48814f5ca70679dde2c16
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-18 18:46:04 -0500

    Update CB.cs

--- CB.cs
+++ CB.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestProject1
 {
-    public class CB : Player
+    public class S : Player
     {
         public int Interceptions_Caught = 0;
         public int Tackels = 0;
@@ -15,9 +15,8 @@ namespace TestProject1
         public int Fumbles_Caused = 0;
         public int Fumble_Recoveries = 0;
         public int Sacks = 0;
-        public int Fantasy_Points = 0;
 
-        public CB(int interceptions_Caught, int tackels, int tackle_Assists, int defensive_Touchdowns, int fumbles_Caused, int fumble_Recoveries, int sacks, int fantasy_Points, string name, string position, int age, int number, string height, int weight, string college)
+        public S(int interceptions_Caught, int tackels, int tackle_Assists, int defensive_Touchdowns, int fumbles_Caused, int fumble_Recoveries, int sacks, string name,  string position,  string age, int number, string height, int weight, string college)
             : base(name, position, age, number, height, weight, college)
         {
             this.Interceptions_Caught = interceptions_Caught;
@@ -27,7 +26,7 @@ namespace TestProject1
             this.Fumbles_Caused = fumbles_Caused;
             this.Fumble_Recoveries = fumble_Recoveries;
             this.Sacks = sacks;
-            this.Fantasy_Points = fantasy_Points;
+            
         }
         public int DBInt{
             get{return Interceptions_Caught;}
@@ -58,4 +57,5 @@ namespace TestProject1
             set{Sacks = value;}
         }
     }
+
 }
