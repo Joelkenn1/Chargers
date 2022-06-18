38e04e02e258066ce05407e3c2326cba5ba5962e
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-18 18:39:14 -0500

    Update QB.cs

--- QB.cs
+++ QB.cs
@@ -9,15 +9,15 @@ namespace TestProject1
     public class QB : Player
     {
         public int Yards = 0;
-        public decimal CompletionPercentage = 0;
+        public int CompletionPercentage = 0;
         public int Completions = 0;
         public int PassingTDs = 0;
         public int RushingTDs = 0;
         public int Interceptions_thrown = 0;
-        public decimal QB_rating = 0;
+        public int QB_rating = 0;
         public int Fantasy_Points = 0;
 
-        public QB(int yards, int completionPercentage, int completions, int passingTDs, int rushingTDs, int interceptions_thrown, int qB_rating, int fantasy_Points, string name, string position, int age, int number, string height, int weight, string college)
+        public QB(int yards, int completionPercentage, int completions, int passingTDs, int rushingTDs, int interceptions_thrown, int qB_rating, int fantasy_Points, string name, string position,  string age, int number, string height, int weight, string college)
             : base(name, position, age, number, height, weight, college)
         {
             this.Yards = yards;
@@ -29,7 +29,7 @@ namespace TestProject1
             this.QB_rating = qB_rating;
             this.Fantasy_Points = fantasy_Points;
         }
-         public int PassYDs{
+        public int PassYDs{
             get{return Yards;}
             set{Yards = value;}
         }
@@ -68,7 +68,6 @@ namespace TestProject1
 
 
         
-
 
     }
 
