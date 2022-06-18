abed308b987ea32a28326154fe8d6f18084be0fe
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-18 18:52:07 -0500

    Update DT.cs

--- DT.cs
+++ DT.cs
@@ -6,25 +6,24 @@ using System.Threading.Tasks;
 
 namespace TestProject1
 {
-    public class DT : Player
+    public class DE : Player
     {
         public int Tackels = 0;
         public int Tackle_Assists = 0;
         public int Fumbles_Caused = 0;
         public int Fumble_Recoveries = 0;
         public int Sacks = 0;
-        public int Fantasy_Points = 0;
+        
 
-        public DT(int tackels, int tackle_Assists, int fumbles_Caused, int fumble_Recoveries, int sacks, int fantasy_Points, string name, string position, int age, int number, string height, int weight, string college)
+        public DE(int tackels, int tackle_Assists, int fumbles_Caused, int fumble_Recoveries, int sacks, string name, string position,  string age, int number, string height, int weight, string college)
             : base(name, position, age, number, height, weight, college)
         {
-
             this.Tackels = tackels;
             this.Tackle_Assists = tackle_Assists;
             this.Fumbles_Caused = fumbles_Caused;
             this.Fumble_Recoveries = fumble_Recoveries;
             this.Sacks = sacks;
-            this.Fantasy_Points = fantasy_Points;
+            
         }
         public int Tkls{
             get{return Tackels;}
@@ -46,5 +45,5 @@ namespace TestProject1
             get{return Sacks;}
             set{Sacks = value;}
         }
-    }
+    }   
 }
