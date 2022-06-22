7e5ecddb1f9f537cdb5f99c3a2359eb107ff84fe
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-22 15:21:40 -0400

    Update DE.cs

--- DE.cs
+++ DE.cs
@@ -13,37 +13,42 @@ namespace TestProject1
         public int Fumbles_Caused = 0;
         public int Fumble_Recoveries = 0;
         public int Sacks = 0;
-        
 
-        public DE(int tackels, int tackle_Assists, int fumbles_Caused, int fumble_Recoveries, int sacks, string name, string position,  string age, int number, string height, int weight, string college)
-            : base(name, position, age, number, height, weight, college)
+
+        public DE(int tackels, int tackle_Assists, int fumbles_Caused, int fumble_Recoveries, int sacks, string name, string position, string age, int number, string height, int weight, string college, string accolades)
+            : base(name, position, age, number, height, weight, college, accolades)
         {
             this.Tackels = tackels;
             this.Tackle_Assists = tackle_Assists;
             this.Fumbles_Caused = fumbles_Caused;
             this.Fumble_Recoveries = fumble_Recoveries;
             this.Sacks = sacks;
-            
+
         }
-        public int Tkls{
-            get{return Tackels;}
-            set{Tackels = value;}
+        public int Tkls
+        {
+            get { return Tackels; }
+            set { Tackels = value; }
         }
-        public int TklAsst{
-            get{return Tackle_Assists;}
-            set{Tackle_Assists = value;}
+        public int TklAsst
+        {
+            get { return Tackle_Assists; }
+            set { Tackle_Assists = value; }
         }
-        public int FumC{
-            get{return Fumbles_Caused;}
-            set{Fumbles_Caused = value;}
+        public int FumC
+        {
+            get { return Fumbles_Caused; }
+            set { Fumbles_Caused = value; }
         }
-        public int FumR{
-            get{return Fumble_Recoveries;}
-            set{Fumble_Recoveries = value;}
+        public int FumR
+        {
+            get { return Fumble_Recoveries; }
+            set { Fumble_Recoveries = value; }
         }
-        public int Sks{
-            get{return Sacks;}
-            set{Sacks = value;}
+        public int Sks
+        {
+            get { return Sacks; }
+            set { Sacks = value; }
         }
-    }   
+    }
 }
