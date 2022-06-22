f65e113dd60121e6a60973b6d61dac1e9986b8ae
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-22 15:20:48 -0400

    Update CB.cs

--- CB.cs
+++ CB.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TestProject1
 {
-    public class S : Player
+    public class CB : Player
     {
         public int Interceptions_Caught = 0;
         public int Tackels = 0;
@@ -16,8 +16,8 @@ namespace TestProject1
         public int Fumble_Recoveries = 0;
         public int Sacks = 0;
 
-        public S(int interceptions_Caught, int tackels, int tackle_Assists, int defensive_Touchdowns, int fumbles_Caused, int fumble_Recoveries, int sacks, string name,  string position,  string age, int number, string height, int weight, string college)
-            : base(name, position, age, number, height, weight, college)
+        public CB(int interceptions_Caught, int tackels, int tackle_Assists, int defensive_Touchdowns, int fumbles_Caused, int fumble_Recoveries, int sacks, string name, string position, string age, int number, string height, int weight, string college, string accolades)
+            : base(name, position, age, number, height, weight, college, accolades)
         {
             this.Interceptions_Caught = interceptions_Caught;
             this.Tackels = tackels;
@@ -26,35 +26,42 @@ namespace TestProject1
             this.Fumbles_Caused = fumbles_Caused;
             this.Fumble_Recoveries = fumble_Recoveries;
             this.Sacks = sacks;
-            
+
         }
-        public int DBInt{
-            get{return Interceptions_Caught;}
-            set{Interceptions_Caught = value;}
+        public int DBInt
+        {
+            get { return Interceptions_Caught; }
+            set { Interceptions_Caught = value; }
         }
-        public int Tkls{
-            get{return Tackels;}
-            set{Tackels = value;}
+        public int Tkls
+        {
+            get { return Tackels; }
+            set { Tackels = value; }
         }
-        public int TklAsst{
-            get{return Tackle_Assists;}
-            set{Tackle_Assists = value;}
+        public int TklAsst
+        {
+            get { return Tackle_Assists; }
+            set { Tackle_Assists = value; }
         }
-        public int DefTDs{
-            get{return Defensive_Touchdowns;}
-            set{Defensive_Touchdowns = value;}
+        public int DefTDs
+        {
+            get { return Defensive_Touchdowns; }
+            set { Defensive_Touchdowns = value; }
         }
-        public int FumC{
-            get{return Fumbles_Caused;}
-            set{Fumbles_Caused = value;}
+        public int FumC
+        {
+            get { return Fumbles_Caused; }
+            set { Fumbles_Caused = value; }
         }
-        public int FumR{
-            get{return Fumble_Recoveries;}
-            set{Fumble_Recoveries = value;}
+        public int FumR
+        {
+            get { return Fumble_Recoveries; }
+            set { Fumble_Recoveries = value; }
         }
-        public int Sks{
-            get{return Sacks;}
-            set{Sacks = value;}
+        public int Sks
+        {
+            get { return Sacks; }
+            set { Sacks = value; }
         }
     }
 
