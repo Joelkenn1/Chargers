0711b98a9864203074446411f459054ba42ace85
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-06-22 15:23:58 -0400

    Update RB.cs

--- RB.cs
+++ RB.cs
@@ -15,8 +15,8 @@ namespace TestProject1
         public int Fumbles = 0;
         public int Fantasy_Points = 0;
 
-        public RB(int rushing_attempts, int rushing_yards, int rushing_tds, int receptions, int fumbles, int fantasy_Points, string name, string position,  string age, int number, string height, int weight, string college)
-        : base(name, position, age, number, height, weight, college)
+        public RB(int rushing_attempts, int rushing_yards, int rushing_tds, int receptions, int fumbles, int fantasy_Points, string name, string position, string age, int number, string height, int weight, string college, string accolades)
+        : base(name, position, age, number, height, weight, college, accolades)
         {
             this.Rushing_Attempts = rushing_attempts;
             this.Rushing_Yards = rushing_yards;
@@ -25,30 +25,36 @@ namespace TestProject1
             this.Fumbles = fumbles;
             this.Fantasy_Points = fantasy_Points;
         }
-        public int Rushatt{
-            get{return Rushing_Attempts;}
-            set{Rushing_Attempts = value;}
+        public int Rushatt
+        {
+            get { return Rushing_Attempts; }
+            set { Rushing_Attempts = value; }
         }
-        public int RushYds{
-            get{return Rushing_Yards;}
-            set{Rushing_Yards = value;}
+        public int RushYds
+        {
+            get { return Rushing_Yards; }
+            set { Rushing_Yards = value; }
         }
-        public int RushTDs{
-            get{return Rushing_TDs;}
-            set{Rushing_TDs = value;}
+        public int RushTDs
+        {
+            get { return Rushing_TDs; }
+            set { Rushing_TDs = value; }
         }
-        public int Rec{
-            get{return Receptions;}
-            set{Receptions = value;}
+        public int Rec
+        {
+            get { return Receptions; }
+            set { Receptions = value; }
         }
 
-        public int Fmbs{
-            get{return Fumbles;}
-            set{Fumbles = value;}
+        public int Fmbs
+        {
+            get { return Fumbles; }
+            set { Fumbles = value; }
         }
-        public int FP{
-            get{return Fantasy_Points;}
-            set{Fantasy_Points = value;}
+        public int FP
+        {
+            get { return Fantasy_Points; }
+            set { Fantasy_Points = value; }
         }
     }
 }
